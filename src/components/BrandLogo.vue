ce8301c58fbf9fbe91c0009b04976ffa953c41a1
laraib07 <a.laraib@protonmail.com>
2024-02-24 21:47:08 +0530

    added partials and components

---
+++ src/components/BrandLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex-1 text-3xl font-bold">
+    <!-- Brand logo -->
+    <button id="brandlogo" class="flex">
+      Laraib
+      <div class="relative">
+        <em class="absolute left-1 text-neutral font-black -z-10">07</em>
+        <em class="text-primary font-black">07</em>
+      </div>
+    </button>
+  </div>
+</template>

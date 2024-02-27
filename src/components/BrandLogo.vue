4fe5ced45002432e4d84661c902e1f736c86ddcb
laraib07 <a.laraib@protonmail.com>
2024-02-27 22:33:43 +0530

    update brand logo

--- src/components/BrandLogo.vue
+++ src/components/BrandLogo.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="flex-1 text-3xl font-bold">
     <!-- Brand logo -->
-    <button id="brandlogo" class="flex">
+    <button
+      id="brandlogo"
+      class="flex"
+      style="font-family: 'Lobster', sans-serif"
+    >
       Laraib
-      <div class="relative">
-        <em class="absolute left-1 text-neutral font-black -z-10">07</em>
-        <em class="text-primary font-black">07</em>
+      <div
+        class="relative font-black"
+        style="font-family: 'Montserrat', sans-serif"
+      >
+        <em class="absolute left-1 text-neutral -z-10">07</em>
+        <em class="text-primary">07</em>
       </div>
     </button>
   </div>

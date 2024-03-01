e4937aa9d79f6559853cab4429f7fcbc1f4b4116
laraib07 <a.laraib@protonmail.com>
2024-03-01 15:31:38 +0530

    refactor about

--- src/partials/About.vue
+++ src/partials/About.vue
@@ -7,12 +7,10 @@ import Leetcode from "../components/icons/Leetcode.vue";
 <template>
   <section id="about" class="w-full px-4 py-16 bg-base-200">
     <div class="flex flex-col mx-auto max-w-screen-xl lg:flex-row lg:w-[90vw]">
-      <div
-        class="w-full max-h-[400px] bg-neutral-700 rounded-lg overflow-hidden lg:w-1/2"
-      >
+      <div class="max-h-[400px] rounded-lg overflow-hidden lg:w-1/2">
         <img src="/images/my_image.webp" alt="Laraib's image" />
       </div>
-      <div class="w-full pt-8 lg:w-1/2 lg:pl-8">
+      <div class="pt-8 lg:pl-8 lg:w-1/2">
         <h1 class="text-4xl font-semibold sm:text-6xl">About Me</h1>
         <p class="pt-6">
           Web developer specializing in full-stack development using Laravel,

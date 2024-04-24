3568d380625cba7a897fdde52e25a9059b6df91d
laraib07 <a.laraib@protonmail.com>
2024-04-24 23:29:15 +0530

    fix experience section layout

--- src/partials/Experience.vue
+++ src/partials/Experience.vue
@@ -11,9 +11,11 @@ defineProps({
 
 <template>
   <section id="experience" class="w-full px-4 py-16 bg-base-300">
-    <div class="flex flex-col mx-auto max-w-screen-lg lg:flex-row">
-      <h1 class="inline text-4xl font-semibold sm:text-6xl">Experience</h1>
-      <div class="pt-6 lg:pt-0 lg:pl-16 lg:w-3/4">
+    <div class="grid gap-6 mx-auto max-w-screen-lg lg:grid-cols-5">
+      <h1 class="text-4xl font-semibold sm:text-6xl lg:col-span-2">
+        Experience
+      </h1>
+      <div class="lg:col-span-3">
         <ExperienceItem v-for="exp of experience" :experience="exp" />
       </div>
     </div>

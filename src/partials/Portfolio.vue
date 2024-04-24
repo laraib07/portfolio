4a7fd5b107739294becb592981eace42eb6e9f9f
laraib07 <a.laraib@protonmail.com>
2024-04-24 23:48:41 +0530

    refactor portfolio

--- src/partials/Portfolio.vue
+++ src/partials/Portfolio.vue
@@ -13,21 +13,21 @@ defineProps({
   <section id="portfolio" class="w-full px-4 py-16 bg-base-200">
     <div class="mx-auto max-w-screen-lg">
       <h1 class="text-4xl font-semibold sm:text-6xl">Portfolio</h1>
-      <div class="grid grid-cols-1 gap-8 pt-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div
+        class="grid gap-8 pt-6 place-items-center sm:grid-cols-2 lg:grid-cols-3"
+      >
         <ProjectItem v-for="project of projects" :project="project" />
 
-        <div class="flex items-center justify-center">
-          <a
-            href="https://github.com/laraib07"
-            target="_blank"
-            class="group text-blue-500 hover:underline"
-          >
-            See more
-            <span class="inline-block duration-200 group-hover:translate-x-2"
-              >&nbsp;&#10230;
-            </span>
-          </a>
-        </div>
+        <a
+          href="https://github.com/laraib07"
+          target="_blank"
+          class="group text-blue-500 hover:underline"
+        >
+          See more
+          <span class="inline-block duration-200 group-hover:translate-x-2"
+            >&nbsp;&#10230;
+          </span>
+        </a>
       </div>
     </div>
   </section>

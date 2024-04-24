d315d11af9368e7b73b80adf0bb079b07b90980b
laraib07 <a.laraib@protonmail.com>
2024-04-24 23:45:20 +0530

    fix image container

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -13,11 +13,16 @@ defineProps({
   <div
     class="flex flex-col rounded-md overflow-hidden group bg-base-100 shadow-sm duration-500 ease-in-out hover:shadow-lg"
   >
-    <div class="h-[150px] overflow-hidden">
+    <div class="relative h-[150px] overflow-hidden flex justify-center">
       <img
         :src="project.imgsrc"
         :alt="project.title"
-        class="object-fit ease-in-out duration-500 group-hover:scale-110"
+        class="object-cover w-full blur-sm"
+      />
+      <img
+        :src="project.imgsrc"
+        :alt="project.title"
+        class="absolute object-contain h-full ease-in-out duration-500 group-hover:scale-110"
       />
     </div>
     <div class="p-4 grow">
@@ -27,11 +32,11 @@ defineProps({
       <h2 class="font-medium text-2xl py-2" v-text="project.title"></h2>
       <p class="text-sm" v-text="project.desc"></p>
     </div>
-    <div class="flex flex-row p-4 gap-2">
+    <div class="flex p-4 gap-2">
       <a
         :href="project.gitlink"
         target="_blank"
-        v-show="project.gitlink"
+        v-if="project.gitlink"
         class="btn btn-outline border-2 flex-1"
       >
         Github
@@ -39,7 +44,7 @@ defineProps({
       <a
         :href="project.demo"
         target="_blank"
-        v-show="project.demo"
+        v-if="project.demo"
         class="btn btn-neutral flex-1"
       >
         Visit

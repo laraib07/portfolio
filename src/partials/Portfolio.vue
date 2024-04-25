186b0d61309f5560d6d7febdc5d783b74ccf4dcd
laraib07 <a.laraib@protonmail.com>
2024-04-25 22:58:25 +0530

    HOTFIX: fix unaligned cards

--- src/partials/Portfolio.vue
+++ src/partials/Portfolio.vue
@@ -13,20 +13,20 @@ defineProps({
   <section id="portfolio" class="w-full px-4 py-16 bg-base-200">
     <div class="mx-auto max-w-screen-lg">
       <h2 class="text-4xl font-semibold sm:text-6xl">Portfolio</h2>
-      <div
-        class="grid gap-8 pt-6 place-items-center sm:grid-cols-2 lg:grid-cols-3"
-      >
+      <div class="grid gap-8 mt-4 sm:grid-cols-2 lg:grid-cols-3">
         <ProjectItem v-for="project of projects" :project="project" />
 
         <a
           href="https://github.com/laraib07"
           target="_blank"
-          class="group text-blue-500 hover:underline"
+          class="grid place-items-center"
         >
-          See more
-          <span class="inline-block duration-200 group-hover:translate-x-2"
-            >&nbsp;&#10230;
-          </span>
+          <p class="group text-blue-500 hover:underline">
+            See more
+            <span class="inline-block duration-200 group-hover:translate-x-2"
+              >&nbsp;&#10230;
+            </span>
+          </p>
         </a>
       </div>
     </div>

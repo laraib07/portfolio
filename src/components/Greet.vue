910df45f76a5a73d58d9f1a392d3798c45fd8cd3
laraib07 <a.laraib@protonmail.com>
2024-04-26 22:33:17 +0530

    refactor greet

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -8,13 +8,12 @@ defineProps({
 </script>
 
 <template>
-  <p class="bg-base-100 bg-clip-text">Hi, I am {{ name }}</p>
+  <p>Hi, I am {{ name }}</p>
 </template>
 
 <style scoped>
 p {
-  -webkit-text-stroke: 2.5px
-    var(--fallback-bc, oklch(var(--bc) / var(--tw-bg-opacity)));
+  -webkit-text-stroke: 2.5px;
   -webkit-text-fill-color: var(
     --fallback-b1,
     oklch(var(--b1) / var(--tw-bg-opacity))

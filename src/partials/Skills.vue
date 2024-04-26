299df22e78d260f109ae7593234d566cc0bb51ed
laraib07 <a.laraib@protonmail.com>
2024-04-26 21:56:27 +0530

    increase size of skill item

--- src/partials/Skills.vue
+++ src/partials/Skills.vue
@@ -20,7 +20,7 @@ defineProps({
       <template v-for="skill of skills" :key="skill.title">
         <div class="mt-4">
           <h3 class="text-base font-medium" v-text="skill.title"></h3>
-          <div class="flex flex-wrap gap-1 mt-2 text-xs">
+          <div class="flex flex-wrap gap-1 mt-2 text-base">
             <SkillItem v-for="name of skill.skills" v-text="name"></SkillItem>
           </div>
         </div>
